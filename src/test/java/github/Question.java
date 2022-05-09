77c57717aa29774fae15aaf02780d773082fc1fe
alekseilisenkov <alexlisenkov92@mail.ru>
2022-05-09 16:30:07 +0300

    1.2

--- src/test/java/github/Question.java
+++ src/test/java/github/Question.java
@@ -4,10 +4,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-public class question {
+public class Question {
     @Test
     @DisplayName("searchForSoftAssertions")
     void searchForSoftAssertions() {
