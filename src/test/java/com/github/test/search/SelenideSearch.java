48b91ce09297f15a19a7192db4763fbae24f4ad3
alekseilisenkov <alexlisenkov92@mail.ru>
2022-06-12 00:18:59 +0300

    refactor of structure final v2.1

--- src/test/java/com/github/test/search/SelenideSearch.java
+++ src/test/java/com/github/test/search/SelenideSearch.java
@@ -1,4 +1,4 @@
-package com.github.test;
+package com.github.test.search;
 
 import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
--- src/test/java/com/github/test/search/SelenideTest.java
+++ src/test/java/com/github/test/search/SelenideTest.java
@@ -1,4 +1,4 @@
-package com.github.test;
+package com.github.test.search;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
